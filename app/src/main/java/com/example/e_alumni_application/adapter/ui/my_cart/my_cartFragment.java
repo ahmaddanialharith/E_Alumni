5e1c5caecb7de9cab0af13281a89e70e031e02ad
ahmaddanialharith <68100921+ahmaddanialharith@users.noreply.github.com>
2023-01-11 15:53:31 +0800

    My cart Fragment & connection to database

--- app/src/main/java/com/example/e_alumni_application/adapter/ui/my_cart/my_cartFragment.java
+++ app/src/main/java/com/example/e_alumni_application/adapter/ui/my_cart/my_cartFragment.java
@@ -6,14 +6,63 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.e_alumni_application.R;
+import com.example.e_alumni_application.adapter.MyCartAdapter;
+import com.example.e_alumni_application.models.MyCartModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class my_cartFragment extends Fragment {
+
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+
+
+    RecyclerView recyclerView;
+    MyCartAdapter cartAdapter;
+    List<MyCartModel> cartModelList;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_my_cart, container, false);
+
+       db = FirebaseFirestore.getInstance();
+       auth = FirebaseAuth.getInstance();
+       recyclerView = root.findViewById(R.id.recyclerview);
+       recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+       cartModelList = new ArrayList<>();
+       cartAdapter = new MyCartAdapter(getActivity(),cartModelList);
+       recyclerView.setAdapter(cartAdapter);
+
+       db.collection("AddToCart").document(auth.getCurrentUser().getUid())
+               .collection("CurrentUser").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                   @Override
+                   public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                       if (task.isSuccessful()){
+                           for (DocumentSnapshot documentSnapshot : task.getResult().getDocuments()){
+                               MyCartModel cartModel = documentSnapshot.toObject(MyCartModel.class);
+                               cartModelList.add(cartModel);
+                               cartAdapter.notifyDataSetChanged();
+                           }
+                       }
+
+                   }
+               });
+
+
         return root;
     }
-//this is java file for cartfragment
+
 }
